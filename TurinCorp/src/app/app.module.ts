a068ac4483960ff6f7fdfd4bf8419d548c672e66
Rapha <rhja@uol.com.br>
2020-10-08 06:08:43 -0300

    decoupled backend from frontend

--- TurinCorp/src/app/app.module.ts
+++ TurinCorp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatCardModule }          from "@angular/material/card";
 import { MatGridListModule }      from "@angular/material/grid-list";
 import { MatButtonModule }        from "@angular/material/button";
 import { MatListModule }          from "@angular/material/list";
+import { MatDividerModule }         from "@angular/material/divider";
 
 import { AppRoutingModule }         from './app-routing.module';
 import { AppComponent }             from './app.component';
@@ -16,8 +17,7 @@ import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
 import { HomeComponent }            from './home/home.component';
 import { NavbarComponent }          from './navbar/navbar.component';
 import { SeguroCardComponent }      from './seguro-card/seguro-card.component';
-import { ListaSegurosComponent }  from './home/lista-seguros/lista-seguros.component';
-import { MatDividerModule } from "@angular/material/divider";
+import { ListaSegurosComponent }    from './home/lista-seguros/lista-seguros.component';
 
 @NgModule({
   declarations: [
--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use((req,res,next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS"
+    "GET"
   )
   next();
 });
