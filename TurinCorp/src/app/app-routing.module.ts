2d3d3ef2634a1cacb5b65b122ad67f31619d3d69
Raphael Hungria <rhja@uol.com.br>
2020-10-11 14:05:02 -0300

    removed front end unused form control component

--- TurinCorp/src/app/app-routing.module.ts
+++ TurinCorp/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from 'src/app/components/home/home.component';
-import { CustomFormControlComponent } from 'src/app/components/custom-forms/custom-form-control/custom-form-control/custom-form-control.component';
 import { CustomFormGroupComponent } from 'src/app/components/custom-forms/custom-form-group/custom-form-group/custom-form-group.component';
 
 const routes: Routes = [
-  {path:"", component:HomeComponent},
-  {path:"debug-form", component:CustomFormGroupComponent},
+  {path: "", component: HomeComponent},
+  {path: "debug-form", component: CustomFormGroupComponent},
 ];
 
 @NgModule({
