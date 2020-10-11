7a2e2c5d3f5ef4d04bf5bb10fa1f9662eae15ce4
Raphael Hungria <rhja@uol.com.br>
2020-10-11 14:04:34 -0300

    added route for submiting any generated form

--- backend/src/controllers/seguro.controller.ts
+++ backend/src/controllers/seguro.controller.ts
@@ -5,3 +5,13 @@ export const getSeguroById = (req: Request, res: Response, next: NextFunction) =
   console.log(req.params);
   res.status(200).json({params: req.params, message: "bom dia teste router"});  
 }
+
+export const receiveFormAnswer = (req: Request, res: Response, next: NextFunction) => 
+{
+  console.log(req.body);
+  res.status(201).json({
+    message: "form enviado",
+    statusCode: 201,
+    form: req.body,
+  })  
+}
--- backend/src/routes/seguro.routes.ts
+++ backend/src/routes/seguro.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
-import { getSeguroById } from "src/controllers/seguro.controller";
+import { getSeguroById, receiveFormAnswer } from "src/controllers/seguro.controller";
 
 let router = Router();
 
 router.get('/:seguroId', getSeguroById);
+
+router.post('/answer', receiveFormAnswer);
 
 export { router as seguroRoutes }
